32fdf6c15a64b0500f9dce76497d227bc79c1069
Thiyan <thiyaneng@gmail.com>
2019-10-09 09:28:26 +0530

    Basic environment setuped

---
+++ src/main/java/YANmakes/complain/ComplainApplication.java
@@ -0,0 +1,13 @@
+package YANmakes.complain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplainApplication.class, args);
+	}
+
+}
